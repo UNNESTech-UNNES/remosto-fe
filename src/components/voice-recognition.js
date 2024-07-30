2fcc385a6cc669e33cb39fb782c3884c7c51e763
michaeljonathann <michaeljopanjaitan@gmail.com>
2024-07-30 13:55:58 +0700

    feat: add SpeechSynthesis

--- src/components/voice-recognition.js
+++ src/components/voice-recognition.js
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 export default function VoiceRecognition() {
     const [isListening, setIsListening] = useState(false);
     const recognitionRef = useRef(null);
+    const synthRef = useRef(null);
 
     useEffect(() => {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -22,6 +23,9 @@ export default function VoiceRecognition() {
         recognitionRef.current.onerror = function(event) {
             console.error('Speech recognition error', event.error);
         };
+
+        // Initialize SpeechSynthesis
+        synthRef.current = window.speechSynthesis;
 
         return () => {
             recognitionRef.current.stop();
@@ -42,25 +46,41 @@ export default function VoiceRecognition() {
 
     function handleCommand(text) {
         const lowerText = text.toLowerCase();
+        
+        const synth = synthRef.current;
+        let utterThis = new SpeechSynthesisUtterance();
+        utterThis.lang = 'id-ID';
+
         // Handle commands here...
         if (lowerText.includes("home") || lowerText.includes("menu utama") || lowerText.includes("halaman utama")) {
             window.location.href = '/'; // Navigasi ke halaman utama
         } else if (lowerText.includes("animals") || lowerText.includes("about animals") || lowerText.includes("satwa") || lowerText.includes("tentang satwa") || lowerText.includes("hewan") || lowerText.includes("tentang hewan")) {
             window.location.href = '/animals'; // Navigasi ke /animals
+            utterThis.text = "Berikut adalah satwa-satwa yang ada di Kebun Binatang Gembira Loka";
+            // utterThis.text = "Here are the animals at Gembira Loka Zoo";
         } else if (lowerText.includes("facilities") || lowerText.includes("public facilities") || lowerText.includes("fasilitas") || lowerText.includes("fasilitas publik") || lowerText.includes("hewan") || lowerText.includes("tentang hewan")) {
             window.location.href = '/public-facilities'; // Navigasi ke /public-facilities
+            utterThis.text = "Berikut beberapa fasilitas umum terdekat di area Kebun Binatang Gembira Loka";
         } else if (lowerText.includes("peta") || lowerText.includes("map")) {
             window.location.href = '/maps'; // Navigasi ke /map
+            utterThis.text = "Berikut adalah peta Kebun Binatang Gembira Loka";
         } else if (lowerText.includes("food") || lowerText.includes("food store") || lowerText.includes("makan") || lowerText.includes("tempat makan") || lowerText.includes("eat") || lowerText.includes("eating")) {
             window.location.href = '/food-store'; // Navigasi ke /food-store
+            utterThis.text = "Berikut beberapa tempat makan terdekat di sekitar Kebun Binatang Gembira Loka";
         } else if (lowerText.includes("nearest animal") || lowerText.includes("hewan terdekat") || lowerText.includes("satwa terdekat")) {
             window.location.href = '/animals/nearest'; // Navigasi ke /animals-nearest
+            utterThis.text = "Berikut beberapa hewan terdekat di sekitar Anda";
         } else if (lowerText.includes("nearest facilities") || lowerText.includes("nearest public facilities") || lowerText.includes("fasilitas terdekat") || lowerText.includes("fasilitas publik terdekat") || lowerText.includes("hewan") || lowerText.includes("tentang hewan")) {
             window.location.href = '/public-facilities/nearest'; // Navigasi ke /public-facilities
+            utterThis.text = "Berikut beberapa fasilitas umum terdekat di sekitar Anda";
         } else {
             console.log("Kata kunci tidak dikenali");
             alert("Kata kunci tidak dikenali");
+            utterThis.text = "Kata kunci tidak dikenali";
         }
+
+        // Speak the response
+        synth.speak(utterThis);
     }
     
     return(
